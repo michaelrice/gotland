91ec6264ee86f4a3a2fce70ae9735391ebd8333a
Michael Rice <michael@michaelrice.org>
2013-09-17 07:40:44 +0000

    added overview and overview test

--- tests/apitest.py
+++ tests/apitest.py
@@ -7,10 +7,24 @@ from gotland.rabbit import api
 
 class RabbitApiTests(unittest.TestCase):
 
+    rabbit = api.api()
+
     def test_aliveness(self):
-        rabbit = api.api()
-        alive = rabbit.check_aliveness()
-        self.failUnless(alive)
+        alive = self.rabbit.check_aliveness()
+        self.assertTrue(alive)
+
+    def test_aliveness_fails(self):
+        dead = self.rabbit.check_aliveness(vhost="failme")
+        self.assertFalse(dead)
+
+    def test_overview(self):
+        alive = self.rabbit.get_overview()
+        self.assertIsInstance(alive,dict)
+
+    def test_overview_for_content(self):
+        """This test will fail if youre not using 3.1.5"""
+        info_dict = self.rabbit.get_overview()
+        self.assertDictContainsSubset({"management_version":"3.1.5"},info_dict)
 
 def main():
     unittest.main()
